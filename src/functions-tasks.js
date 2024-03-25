fd2e606447dd8618a393d409d4882d748d9aead0
dn071099umi <mariia.uzlova@privatbank.ua>
2024-03-25 12:15:57 +0200

    feat: add 1 task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -146,8 +146,17 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function xX() {
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (error) {
+        console.error('attempt', i + 1, 'fatal', error.message);
+      }
+    }
+    throw new Error('All error');
+  };
 }
 
 /**
